2092fcf4f51766dfd2002830b5833d4e7dcaf837
alexander-io <abharris@pugetsound.edu>
2017-08-06 23:12:19 -0700

    ++finalize migration of time functions and splitter functions into server side script, now each request from client is served with a copy of the audio file that's split based on their time tuning in

--- mp3.js
+++ mp3.js
@@ -5,13 +5,14 @@ var io = require('socket.io')(server);
 var socket_stream = require('socket.io-stream');
 
 
-const {exec} = require('child_process')
+const {execSync} = require('child_process')
 let date = new Date()
 // define conversion functions
 const hr_to_sec = (hr) => hr*60*60
 const min_to_sec = (min) => min*60
 
 const server_start_time_seconds = hr_to_sec(date.getHours()) + min_to_sec(date.getMinutes()) + date.getSeconds()
+
 console.log('server start time \t\t:', date.toLocaleTimeString() + '\nserver start time seconds \t:', server_start_time_seconds);
 
 /*
@@ -23,6 +24,55 @@ console.log('server start time \t\t:', date.toLocaleTimeString() + '\nserver sta
 let stream_start_time = function() {
   let d = new Date()
   return hr_to_sec(d.getHours()) + min_to_sec(d.getMinutes()) + d.getSeconds()
+}
+
+/*
+ * making a live audio stream, not every listener tunes in at the same time, so I need a way of splitting the audio according to the start time that the user tunes in
+ *
+ * the ffmpeg package has this functionality...
+ * ffmpeg -i <file_to_split>.mp3 -acodec copy -ss <start_time_of_split> -t <end_time_of_split> <new_file_name_for_split>.mp3
+ *
+ * ex :
+ *  ffmpeg -i lofi.mp3 -acodec copy -ss 00:00:15 -t 00:00:15 lofi_half2.mp3
+ *
+ * ... the format of the <start_time_of_split> and <end_time_of_split> is hours:minutes:seconds or 00:00:00
+ *
+ * so, this is a function to take in a number of seconds and return the a string formatted in this way
+ *
+ * ex :
+ *  30 -> 0:0:30 -> thirty seconds
+ *  60 -> 0:1:0 -> one minute
+ *  90 -> 0:1:30 -> one minute and thirty seconds
+ *  120 -> 0:2:0 -> two minutes
+ *  60*60 -> 1:0:0 -> one hour
+ */
+let format_seconds = function(seconds) {
+  let seconds_per_min = 60, seconds_per_hour = 60*60
+
+  // get the number of hours
+  let hrs = seconds / seconds_per_hour
+  hrs = Math.floor(hrs)
+
+  // the the number of minutes
+  let mins = seconds / seconds_per_min
+  mins = mins - (hrs*60)
+  mins = Math.floor(mins)
+
+  // get the number of seconds
+  let secs = seconds - (hrs * seconds_per_hour) - (mins * seconds_per_min)
+
+  return hrs + ':' + mins + ':' + secs
+}
+
+/*
+ * use ffmpeg to split an audio file starting at some time in seconds that's passed in as a parameter
+ *
+ */
+let split_audio = function(start_time_in_seconds) {
+	let formatted_seconds = format_seconds(start_time_in_seconds)
+	console.log('formatted seconds :', formatted_seconds)
+  execSync('ffmpeg -i '+__dirname +'/music/lofi.mp3 -acodec copy -ss ' + formatted_seconds + ' -t 00:00:30 '+__dirname +'/music/lofi_split.mp3')
+
 }
 
 
@@ -54,6 +104,9 @@ app.get('/fonts/roboto/Roboto-Regular.woff', function(req, res) {
   res.sendFile(path.join(__dirname + '/' + 'node_modules/materialize-css/dist/fonts/roboto/Roboto-Regular.woff'))
 })
 
+
+
+
 /*
  * the client will emit the 'connection' event on the io() function call, the call looks like this :
  *  var socket = io('http://localhost:' + window.location.port);
@@ -61,8 +114,21 @@ app.get('/fonts/roboto/Roboto-Regular.woff', function(req, res) {
  *
  */
 io.on('connection', function (socket) {
+  
+  socket.on('disconnect', function(reason) {
+  	console.log('client disconnected :', reason)
+  })
+
   let stream_start_seconds = stream_start_time()
+  let seconds_since_server_start = stream_start_seconds - server_start_time_seconds
   console.log('\n\tclient connect\n\tstart time :', stream_start_seconds);
+  split_audio(seconds_since_server_start)
+
+  // new Promise(function(resolve, reject) {
+  // 	split_audio(stream_start_seconds)
+  // })
+
+  filename = __dirname + '/music/lofi_split.mp3'
 
   // emit start event to client
   socket.emit('start', { hello: 'world' });
@@ -88,6 +154,14 @@ io.on('connection', function (socket) {
      * for each data chunk that's sent, a 'data' event will be emitted
      */
     fs.createReadStream(filename).pipe(stream);
+    stream.on('finish', function() {
+    	console.log('stream finished	')
+    	fs.unlink(__dirname + '/music/lofi_split.mp3', console.log('finish'))
+    })
   });
 
 });
+
+// io.on('disconnect', function(socket) {
+// 	console.log('client has disconnected')
+// })
